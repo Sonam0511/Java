fa185bf1f53f832c6da6579ec836e7a5fc295a3f
Sonam0511 <sonamsingh3351@gmail.com>
2024-10-27 22:43:52 +0530

    Delete SpringApi directory

--- SpringApi/src/main/java/org/example/repository/DepartmentRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.repository;
-
-import org.example.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-}
-
--- SpringApi/src/main/java/org/example/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.repository;
-
-import org.example.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
-
